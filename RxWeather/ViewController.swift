72d8fcb14004687e1c8fe5452e119682b4deb993
ezequiel3434 <ezequiel3434@gmail.com>
2020-12-06 23:39:14 -0300

    fetching and displaying weather on search

--- RxWeather/ViewController.swift
+++ RxWeather/ViewController.swift
@@ -20,7 +20,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.cityNameTextField.rx.value
+        
+        self.cityNameTextField.rx.controlEvent(.editingDidEndOnExit)
+            .asObservable()
+            .map({self.cityNameTextField.text})
             .subscribe(onNext: { city in
                 if let city = city {
                     if city.isEmpty {
@@ -30,6 +33,8 @@ class ViewController: UIViewController {
                     }
                 }
             }).disposed(by: disposeBag)
+        
+        
     }
     
     private func fetchWeather(by city: String){
